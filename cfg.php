67d3f92ef621dd0f64442015f054fee6760fa3ba
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2021-01-29 08:02:36 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,8 @@
+<?php
+$user-agent = "Mozilla/5.0 (Linux; Android 6.0; Lenovo A6600d40) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
+$__cfduid = "de9f78b8c0dba7015de2c1b90a12b25971610234155";
+$_ga = "GA1.2.145092982.1610234158";
+$_gid = "GA1.2.1005007525.1611847587";
+$cf_clearance = "7be86a92980f026338d106396197cb2f114d2916-1611879584-0-150";
+$PHPSESSID = "ba518oq6m6p79cvg9oo9i86n21";
+$_gat_gtag_UA_162747370_1 = "99999";
